107b9e1d62f7e089d825e05e86193304b866915c
Julio Serra <julioserra@protonmail.com>
2025-02-17 15:05:53 +0100

    update areas component

--- src/components/Areas.jsx
+++ src/components/Areas.jsx
@@ -9,24 +9,24 @@ export default function AccordionSection() {
 
   return (
     <div className="bg-primary">
-      <div className="py-10 max-w-screen-xl mx-auto">
-        <h2 className="text-white text-center text-2xl md:text-4xl md:text-left">{title}</h2>
-        <div className="pt-10 px-5 flex flex-col md:grid md:grid-cols-2 md:gap-10">
+      <div className="py-10 max-w-screen-xl mx-auto px-4">
+        <h2 className="text-white text-center text-2xl lg:text-4xl lg:text-left">{title}</h2>
+        <div className="pt-10 flex flex-col lg:grid lg:grid-cols-2 lg:gap-10">
           <img
             src={ACCORDIONS[activeIndex].image}
             alt=""
-            className="hidden md:block rounded-xl object-cover object-center transition-opacity duration-300 ease-in"
+            className="hidden lg:block rounded-xl object-cover object-center transition-opacity duration-300 ease-in"
           />
           <div className="flex flex-col gap-4">
             {ACCORDIONS.map(({ number, title, description }, index) => (
               <div key={number} className={`flex flex-col ${activeIndex === index ? 'gap-4' : ''}`}>
                 <button
                   type="button"
-                  className="hover:cursor-pointer dark:bg-primary border-t-1 md:border-t-white pt-5 flex items-center gap-3 justify-start w-full font-medium"
+                  className="hover:cursor-pointer dark:bg-primary border-t-1 lg:border-t-white pt-5 flex items-center gap-3 justify-start w-full font-medium"
                   onClick={() => setActiveIndex(index)}
                 >
                   <span className="text-slate-400 text-sm">{number}</span>
-                  <p className="text-white">{title}</p>
+                  <h2 className="text-white text-xl lg:text-2xl">{title}</h2>
                 </button>
                 <div
                   className={`transition duration-500 ease-in overflow-hidden ${activeIndex === index ? 'opacity-100 translate-y-0 h-auto' : 'opacity-0 -translate-y-4 h-0'}`}
@@ -34,9 +34,9 @@ export default function AccordionSection() {
                   <img
                     src={ACCORDIONS[activeIndex].image}
                     alt=""
-                    className="rounded-xl md:w-3xs xl:w-96 md:hidden transition-opacity duration-500 ease-in"
+                    className="rounded-xl w-full md:mx-auto md:w-2xs lg:hidden transition-opacity duration-500 ease-in"
                   />
-                  <p className="text-white text-sm mt-3">{description}</p>
+                  <p className="text-white text-xs md:text-sm mt-3">{description}</p>
                 </div>
               </div>
             ))}
