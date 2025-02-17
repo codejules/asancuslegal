5e663bdc857dfb6d276c4f33355a8009e9b378c5
Julio Serra <julioserra@protonmail.com>
2025-02-17 15:52:14 +0100

    update soluciones component; create type solutions

---
+++ src/types/solutions.ts
@@ -0,0 +1,27 @@
+export type Solutions = {
+    percentage: string;
+    description: string;
+}
+
+export const SOLUTIONS: Solutions[] = [
+    {
+        percentage: "30%",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    },
+    {
+        percentage: "up to 28%",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    },
+    {
+        percentage: "up to 33%",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    },
+    {
+        percentage: "up to 55%",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    },
+];
