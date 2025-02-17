c983e49a90c963638ead154fc8b5e9c2e324900b
Julio Serra <julioserra@protonmail.com>
2025-02-17 10:08:10 +0100

    update menu burger; create types

---
+++ src/types/nav.ts
@@ -0,0 +1,19 @@
+export type Nav = {
+    title: string;
+    url: string;
+}
+
+export const NAV: Nav[] = [
+    {
+        title: "Servicios",
+        url: "/areas",
+    },
+    {
+        title: "Soluciones",
+        url: "/soluciones",
+    },
+    {
+        title: "Contacto",
+        url: "/contacto",
+    },
+];
