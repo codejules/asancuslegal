5abed0e70b36d0dc295a894f392c6f39cf844e2e
Julio Serra <julioserra@protonmail.com>
2025-02-18 16:27:37 +0100

    create json to services

--- src/components/Areas.jsx
+++ src/components/Areas.jsx
@@ -1,9 +1,9 @@
 import { useState } from "preact/hooks";
-import { ACCORDIONS } from "@/types/accordion";
+import SERVICIOS from "@/data/servicios.json"; // Asegúrate de que la ruta sea correcta
 
 const title = "Nuestras principales Áreas";
 
-export default function AccordionSection() {
+export default function Servicios() {
   const [activeIndex, setActiveIndex] = useState(0);
 
   return (
@@ -12,13 +12,13 @@ export default function AccordionSection() {
         <h2 className="badge">{title}</h2>
         <div className="pt-10 flex flex-col lg:grid lg:grid-cols-2 lg:gap-10">
           <img
-            src={ACCORDIONS[activeIndex].image}
+            src={SERVICIOS[activeIndex].image}
             alt=""
             className="hidden lg:block rounded-xl object-cover object-center transition-opacity duration-300 ease-in"
           />
           <div className="flex flex-col gap-4">
-            {ACCORDIONS.map(({ number, title, description }, index) => (
-              <div key={number} className={`flex flex-col ${activeIndex === index ? 'gap-4' : ''} border-t-1 lg:border-t-white `}>
+            {SERVICIOS.map(({ number, title, descriptions }, index) => (
+              <div key={number} className={`flex flex-col ${activeIndex === index ? 'gap-4' : ''} border-t-1 lg:border-t-white`}>
                 <button
                   type="button"
                   className="hover:scale-95 transition duration-300 ease-in cursor-pointer dark:bg-primary pt-5 flex items-center gap-3 justify-start w-full font-medium"
@@ -31,11 +31,15 @@ export default function AccordionSection() {
                   className={`transition duration-500 ease-in overflow-hidden ${activeIndex === index ? 'opacity-100 translate-y-0 h-auto' : 'opacity-0 -translate-y-4 h-0'}`}
                 >
                   <img
-                    src={ACCORDIONS[activeIndex].image}
+                    src={SERVICIOS[activeIndex].image}
                     alt=""
                     className="rounded-xl w-full md:mx-auto md:w-2xs lg:hidden transition-opacity duration-500 ease-in"
                   />
-                  <p className="text-white text-xs md:text-sm mt-3">{description}</p>
+                  <div className="flex flex-col gap-3 px-7">
+                    {descriptions.map((description, i) => (
+                      <p key={i} className="text-white text-xs md:text-sm">{description}</p>
+                    ))}
+                  </div>
                 </div>
               </div>
             ))}
