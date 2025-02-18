eb5d7516bcb2699ce09fb59f7589fee8226ba260
Julio Serra <julioserra@protonmail.com>
2025-02-18 11:45:57 +0100

    update route images

--- src/types/accordion.ts
+++ src/types/accordion.ts
@@ -9,14 +9,14 @@ export const ACCORDIONS: Accordions[] = [
     {
         number: "01",
         title: "Área Fiscal",
-        image: "/src/assets/img/area-fiscal.webp",
+        image: "/assets/img/area-fiscal.webp",
         description:
             "Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and start developing websites even faster with components on top of Tailwind CSS.",
     },
     {
         number: "02",
         title: "Área Mercantil",
-        image: "/src/assets/img/area-mercantil.webp",
+        image: "/assets/img/area-mercantil.webp",
         description:
             "Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and start developing websites even faster with components on top of Tailwind CSS.",
     },
