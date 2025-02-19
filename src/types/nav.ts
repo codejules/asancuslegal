15194a3e11d41d06c2fbab01659c325c7d12fa74
Julio Serra <julioserra@protonmail.com>
2025-02-19 12:53:03 +0100

    add drawer component to show contacto form; update nav types and menu burger

--- src/types/nav.ts
+++ src/types/nav.ts
@@ -1,6 +1,13 @@
 export type Nav = {
+    id?: string;
     title: string;
     url: string;
+    target?: string;
+    show?: string;
+    controls?: string;
+    placement?: string;
+    edge?: string;
+    offset?: string;
 }
 
 export const NAV: Nav[] = [
@@ -17,7 +24,14 @@ export const NAV: Nav[] = [
         url: "#equipo",
     },
     {
+        id: 'contacto',
         title: "Contacto",
         url: "#contacto",
+        target: "drawer-swipe",
+        show: "drawer-swipe",
+        controls: "drawer-swipe",
+        placement: "bottom",
+        edge: "true",
+        offset: "bottom-[30px]"
     },
 ];
