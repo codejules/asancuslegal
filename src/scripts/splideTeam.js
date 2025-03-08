6e7c31387dcccb4a13de899e0b0c77f19821920a
Julio Serra <julioserra@protonmail.com>
2025-03-08 10:30:41 +0100

    add logos and favicon; update header and layout component; update splidejs

--- src/scripts/splideTeam.js
+++ src/scripts/splideTeam.js
@@ -6,15 +6,12 @@ export function initSlider() {
     if (sliderElement && !sliderElement.splide) {
         new Splide(sliderElement, {
             type: "loop",
-            perPage: 1,
-            autoplay: true,
-            interval: 3000,
-            pauseOnHover: true,
+            drag: "free",
+            gap: ".5rem",
+            focus: "center",
+            perPage: 2,
+            width: "100%",
             arrows: false,
-            pagination: false,
-            cover: true,
-            height: "550px",
-            width: "400px",
         }).mount();
     }
 }
