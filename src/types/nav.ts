17aadbf640b7990985d14ad6b5df0d545e6f46d3
Julio Serra <julioserra@protonmail.com>
2025-03-11 17:50:32 +0100

    add styles aviso-legal component; update url links nav

--- src/types/nav.ts
+++ src/types/nav.ts
@@ -9,10 +9,10 @@ export function getNavItems(currentLocale: string): NavItems[] {
     const i18n = getI18N({ currentLocale });
     
     return [
-        { title: i18n.NAV_SERVICES, href: "#areas" },
-        { title: i18n.NAV_SOLUTIONS, href: "#soluciones" },
-        { title: i18n.NAV_TEAM, href: "#equipo" },
-        { title: i18n.NAV_CONTACT, href: "#contacto" },
+        { title: i18n.NAV_SERVICES, href: "/#areas" },
+        { title: i18n.NAV_SOLUTIONS, href: "/#soluciones" },
+        { title: i18n.NAV_TEAM, href: "/#equipo" },
+        { title: i18n.NAV_CONTACT, href: "/#contacto" },
     ];
 }
 
