46369a7f3fb2fa4e1c66e4752de7603553da1091
Julio Serra <julioserra@protonmail.com>
2025-03-13 17:46:46 +0100

    refactor solutions function

--- src/types/solutions.ts
+++ src/types/solutions.ts
@@ -1,27 +1,16 @@
+import { getI18N } from "@/i18n";
+
 export type Solutions = {
     percentage: string;
     description: string;
 }
+export function SOLUTIONS(currentLocale: any): Solutions[] {
+    const i18n = getI18N({ currentLocale });
 
-export const SOLUTIONS: Solutions[] = [
-    {
-        percentage: "30%",
-        description:
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-    },
-    {
-        percentage: "up to 28%",
-        description:
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-    },
-    {
-        percentage: "up to 33%",
-        description:
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-    },
-    {
-        percentage: "up to 55%",
-        description:
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-    },
-];
+    return [
+        { percentage: i18n.SOLUTIONS_PERCENTAGE_1, description: i18n.SOLUTIONS_DESCRIPTION_1 },
+        { percentage: i18n.SOLUTIONS_PERCENTAGE_2, description: i18n.SOLUTIONS_DESCRIPTION_2 },
+        { percentage: i18n.SOLUTIONS_PERCENTAGE_3, description: i18n.SOLUTIONS_DESCRIPTION_3 },
+        { percentage: i18n.SOLUTIONS_PERCENTAGE_4, description: i18n.SOLUTIONS_DESCRIPTION_4 },
+    ]
+};
