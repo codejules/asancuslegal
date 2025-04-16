2594621e56c7078815872c4106bdb3d2785b8632
Julio Serra <julioserra@protonmail.com>
2025-04-16 13:13:00 +0200

    Update cdn utils and path

--- src/utils/cdn.ts
+++ src/utils/cdn.ts
@@ -1,5 +1,6 @@
 const CDN_DOMAIN = import.meta.env.CDN_DOMAIN
 
 export const cdn = (path: string): string => {
-  return `${CDN_DOMAIN}${path.startsWith('/') ? path : `/${path}`}`;
+  const cleanPath = path.startsWith('/') ? path : `/${path}`;
+  return `${CDN_DOMAIN}${cleanPath}`;
 };
