55ea001befd7eb59020dd75c6330ec9915a5599f
Julio Serra <julioserra@protonmail.com>
2025-04-17 10:12:20 +0200

    Add url cdn images in path images

--- src/types/layout.ts
+++ src/types/layout.ts
@@ -1,3 +1,5 @@
+import { CDN_IMAGES } from "@/utils/cdn";
+
 export type Favicon = { url: string; media: string };
 export const FAVICON: Favicon[] = [
 	{
@@ -13,17 +15,17 @@ export const FAVICON: Favicon[] = [
 export type Hero = { url: string; media: string };
 export const HERO: Hero[] = [
 	{
-		url: "/assets/img/asancuslegal-hero.webp",
+		url: `${CDN_IMAGES}/assets/img/asancuslegal-hero.webp`,
 		media: "(min-width: 768px)",
 	},
 	{
-		url: "/assets/img/asancuslegal-hero-mobile.webp",
+		url: `${CDN_IMAGES}/assets/img/asancuslegal-hero-mobile.webp`,
 		media: "(max-width: 767px)",
 	},
 ];
 
 export type TeamImg = { url: string };
 export const TEAM_IMG: TeamImg[] = [
-	{ url: "/assets/img/sandra-serra-asancus-legal.webp" },
-	{ url: "/assets/img/alejandro-toledo-asancus-legal.webp" },
+	{ url: `${CDN_IMAGES}/assets/img/sandra-serra-asancus-legal.webp` },
+	{ url: `${CDN_IMAGES}/assets/img/alejandro-toledo-asancus-legal.webp` },
 ];
