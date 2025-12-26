ea3cc912c908e59e44d88c8d8be0cfadea3283a1
Julio Serra <julioserra@protonmail.com>
2025-12-26 12:55:31 +0100

    Add currentLocale in servicios component

--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -13,7 +13,7 @@ export default function Servicios({ currentLocale }) {
   }));
 
   return (
-    <div id="servicios" className="bg-primary">
+    <div id={currentLocale === 'es' ? 'servicios' : 'services'} className="bg-primary">
       <div className="py-10 lg:py-20 max-w-screen-xl mx-auto px-4">
         <h2 className="badge font-semibold">{i18n.AREAS_TITLE}</h2>
         <div className="pt-10 flex flex-col lg:grid lg:grid-cols-2 lg:gap-10">
